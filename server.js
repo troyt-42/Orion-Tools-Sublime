8b98446d4350da62cd603b018f2cd528a24a80dd
Tao, Zeyu <rnm2888@gmail.com>
2016-02-10 17:18:05 -0500

    Send first request to ternHttpServer

--- server.js
+++ server.js
@@ -3,9 +3,22 @@
 var request = require("request");
 // console.log(orion.infer.findExpressionAround);
 // console.log(ternHttpServer);
+
+var requestObj = {
+					'files': [
+						{
+							'text': '// var orion = require("./OrionJavaScript.js");\n// var ternHttpServer = require("./node_modules/tern/bin/tern");\nvar request = require("request");\n// console.log(orion.infer.findExpressionAround);\n// console.log(ternHttpServer);\n\nvar requestObj = {\n\t"files": [\n\t\t{\t"text": "functoin test(a, b){console.log(a,b)}",\n\t\t\t"name": "test.js", \n\t\t\t"type": "part"\n\t\t}\n\t]\n};\n\n\n\nrequest\n\t.post("http://localhost:60356", requestObj)\n\t.on("response", function(response){\n\t\tconsole.log("test");\n\t\tconsole.log(response.statusCode);\n\t});\n', 
+							'name': 'server.js', 
+							'type': 'full'
+						}
+	]
+};
+
+
+
 request
-	.post("http://localhost:60356")
+	.post({url:"http://localhost:60356/", body: JSON.stringify(requestObj)})
 	.on("response", function(response){
 		console.log("test");
 		console.log(response.statusCode);
-	})
+	});
