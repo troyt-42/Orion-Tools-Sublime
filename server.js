68b51692ef0dbe5acea39f3c8efeaa07da143ed4
Tao, Zeyu <rnm2888@gmail.com>
2016-02-11 10:28:32 -0500

    Test "definition" query

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ var request = require("request");
 // console.log(ternHttpServer);
 
 var requestObj = {
-					'files': [
-						{
-							'text': '// var orion = require("./OrionJavaScript.js");\n// var ternHttpServer = require("./node_modules/tern/bin/tern");\nvar request = require("request");\n// console.log(orion.infer.findExpressionAround);\n// console.log(ternHttpServer);\n\nvar requestObj = {\n\t"files": [\n\t\t{\t"text": "functoin test(a, b){console.log(a,b)}",\n\t\t\t"name": "test.js", \n\t\t\t"type": "part"\n\t\t}\n\t]\n};\n\n\n\nrequest\n\t.post("http://localhost:60356", requestObj)\n\t.on("response", function(response){\n\t\tconsole.log("test");\n\t\tconsole.log(response.statusCode);\n\t});\n', 
-							'name': 'server.js', 
-							'type': 'full'
-						}
-	]
+	'files': [
+		{	'text': '\t\t], __WEBPACK_AMD_DEFINE_RESULT__ = function() {\n\t\t\t\n\t\t\t/**\n\t\t\t * @description Returns if the given character is upper case or not considering the locale\n\t\t\t * @param {String} string A string of at least one char14acter\n\t\t\t * @return {Boolean} True iff the first character of the given string is uppercase\n\t\t\t */\n\t\t\t function isUpperCase(string) {\n\t\t\t\tif (string.length < 1) {\n\t\t\t\treturn false;\n\t\t\t\t}\n\t\t\t\tif (isNaN(string.charCodeAt(0))) {\n\t\t\t\t\treturn false;\n\t\t\t\t}\n\t\t\t\treturn string.toLocaleUpperCase().charAt(0) === string.charAt(0);\n\t\t\t}\n\t\t\t\n\t\t\t/**\n\t\t\t * @description Match ignoring case and checking camel case.\n\t\t\t * @param {String} prefix\n\t\t\t * @param {String} target\n\t\t\t * @returns {Boolean} If the two strings match\n\t\t\t */\n\t\t\tfunction looselyMatches(prefix, target) {\n\t\t\t\tif (typeof prefix !== "string" || typeof target !== "string") {\n\t\t\t\t\treturn false;\n\t\t\t\t}\n\t', 
+			'name': 'OrionJavaScript.js', 'type': 'part', 'offset': 263533
+		}
+	], 
+	'query': {
+		'file': '#0', 
+		'lineCharPositions': true, 
+		'type': 'definition', 
+		'end': 370
+	}
 };
 
 
@@ -20,5 +24,11 @@ request
 	.post({url:"http://localhost:60356/", body: JSON.stringify(requestObj)})
 	.on("response", function(response){
 		console.log("test");
-		console.log(response.statusCode);
+		var resObj = '';
+	    response.on('data', function(data) {
+	      resObj += data;
+	    });
+	    response.on('end', function(){
+	    	console.log(JSON.parse(resObj));
+	    });
 	});
